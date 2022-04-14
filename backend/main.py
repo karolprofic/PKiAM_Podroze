884c438b7fee68ccc0169663df1fd41ccf1f7cfc
Karol Profic <karolprofic@protonmail.com>
2022-04-14 02:47:12 +0200

    Zarys API

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,66 @@ import json
 from covid import getCovidStatistics
 from weather import getWeatherForecast
 from helpers import distanseBeetweenTwoPoints
+from flask import Flask, request, jsonify
+from markupsafe import escape
+
+app = Flask(__name__)
+
+# Uwierzytelnianie
+# @app.route("/login")
+# def login():
+#     return jsonify({'Tresc': "Logowanie"})
+#
+# @app.route("/logout")
+# def logout():
+#     return jsonify({'Tresc': "Wylogowywanie"})
+
+# Użytkownik
+@app.route('/users/<user_id>', methods = ['GET', 'POST', 'PUT', 'DELETE'])
+def users(user_id):
+    if request.method == 'GET':
+        return jsonify({'Tresc': "Zwrócono dane uzytkownika"})
+    if request.method == 'POST':
+        # username = request.form.get('username')
+        # password = request.form.get('password')
+        # city = request.form.get('city')
+        # currency = request.form.get('currency')
+        return jsonify({'Tresc': "Uaktualnienie danych uzytkownika"})
+    if request.method == 'PUT':
+        return jsonify({'Tresc': "Dodano nowego uzytkownika"})
+    if request.method == 'DELETE':
+        return jsonify({'Tresc': "Usunieto uzytkownika"})
+    else:
+        return jsonify({'Tresc': "Błąd"})
+
+# Ulubione
+@app.route('/favourites/<user_id>', methods = ['GET', 'PUT', 'DELETE'])
+def favourites(user_id):
+    if request.method == 'GET':
+        return jsonify({'Tresc': "Zwrócono wszystkie ulubione miejsca dla konkretnego uzytkownika"})
+    if request.method == 'PUT':
+        return jsonify({'Tresc': "Dodano miejsce do ulubionych uzytkownika"})
+    if request.method == 'DELETE':
+        return jsonify({'Tresc': "Usunięto miejsce z ulubionych uzytkownika"})
+    else:
+        return jsonify({'Tresc': "Błąd"})
+
+# Strana główna
+@app.route("/availableCities/")
+def availableCities():
+    return jsonify({'Tresc': "Strona główna"})
+
+# Lista potencjalnych kierunków
+@app.route("/travelDestinations/")
+def travelDestinations():
+    startDate = request.args.get('startDate', None)
+    endDate = request.args.get('endDate', None)
+    startingCity = request.args.get('startingCity', "Łódź")
+    numberOfPeople = request.args.get('numberOfPeople', 1)
+    pageNumber = request.args.get('pageNumber', 1)
+    return jsonify({'Miasto startowe': startingCity})
+
+
 
 # print(json.dumps(getCovidStatistics("Ghana"), indent=2))
 # print(json.dumps(getWeatherForecast(10, 37.773972, -122.431297), indent=2)) # los angeles
