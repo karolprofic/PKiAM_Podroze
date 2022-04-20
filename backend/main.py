c084f9cec9604e26a00d1d673cf0bff30c68a17a
Karol Profic <karolprofic@protonmail.com>
2022-04-20 13:43:27 +0200

    Dodanie GET / DEL users

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,24 @@
 import json
+
+from _mysql_connector import MySQL
+
 from Get.covid import getCovidStatistics
 from flask import Flask, request, jsonify
 from destinations import getTravelDestinations
+import mysql.connector
 
 app = Flask(__name__)
+
+def databse_connect():
+    mysql_db = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="travel_app"
+    )
+
+    return mysql_db
+
 #######################################################################
 # TODO
 #######################################################################
@@ -13,9 +28,9 @@ app = Flask(__name__)
 # - token is valid
 #######################################################################
 # Ulubione
-# - PUT | Dodaj do ulubionych (userID, cityData)
-# - DEL | Usuń z ulubionych (userID, cityIdentyficator)
-# - GET | Pobierz wszystko z ulubionych (userID)
+# - POST | Dodaj do ulubionych (userID, cityData)
+# - DEL  | Usuń z ulubionych (userID, cityIdentyficator)
+# - GET  | Pobierz wszystko z ulubionych (userID)
 #######################################################################
 # Użytkownik
 # - GET  | Zwróć dane użytkownika (userID)
@@ -23,6 +38,43 @@ app = Flask(__name__)
 # - DEL  | Usuń użytkownika (userID)
 # - POST | Uaktualnij dane użytkownika (userID, userData)
 #######################################################################
+
+# CREATE TABLE `travel_app`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `name` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL , `surname` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL , `city` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL , `currency` VARCHAR(3) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL , `avatar` VARCHAR(1024) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+# CREATE TABLE `travel_app`.`favorites` ( `id` INT NOT NULL AUTO_INCREMENT , `user_id` INT NOT NULL , `city` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+# INSERT INTO `users` (`id`, `name`, `surname`, `city`, `currency`, `avatar`) VALUES (NULL, 'Jan', 'Kowalski', 'Łódź', 'PLN', 'https://cdn.pixabay.com/photo/2018/08/28/12/41/avatar-3637425__340.png');
+
+
+@app.route("/favorites/", methods=['GET', 'POST', 'DEL'])
+def favorites():
+    params = None
+    if request.method == 'GET':
+        params = request.json
+    if request.method == 'DEL':
+        params = request.json
+    if request.method == 'POST':
+        params = request.json
+
+
+@app.route("/user/", methods=['GET', 'POST', 'DEL', 'PUT'])
+def user():
+    params = None
+    db = databse_connect()
+    cursor = db.cursor()
+    if request.method == 'GET':
+        params = request.json
+        cursor.execute("SELECT * FROM users WHERE id = " + params.id)
+        results = cursor.fetchall()
+        return jsonify(results)
+    if request.method == 'DEL':
+        params = request.json
+        cursor.execute("DELETE FROM users WHERE id = " + params.id)
+        results = cursor.fetchall()
+        return jsonify(results)
+    if request.method == 'PUT':
+        params = request.json
+    if request.method == 'POST':
+        params = request.json
+
 
 # Strana główna
 @app.route("/availableCities/")
